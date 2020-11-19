50568afe9f928fc28810360660543e8b28dd022a
qian1628 <v-qiwan5@microsoft.com>
2020-11-19 08:36:37 +0000

    Add jit smaple code

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+		public void SurpressJit()
+		{
+			int nouse = 0;
+			for (int i = 0; i < 10; i++)
+			{
+				DataTable dataTableNoUse = new DataTable();
+			}
+		}
+
+	}
+}
---
+++ JITSampleForNexus/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace JITSampleForNexus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl = new Class1();
+
+            cl.SurpressJit(); //set bp here
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
